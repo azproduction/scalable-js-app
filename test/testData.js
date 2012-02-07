65d16a32e6de2055c6723198086ed3ba17381990
azproduction <azazel.private@gmail.com>
2012-02-07 17:41:35 +0600

    removed old test dir

--- test/testData.js
+++
@@ -1,7 +0,0 @@
-var TestData = {
-    "newData": function () {
-        var data = [NaN, Infinity, window, Error, 'pewpewpew', '<b>Pewpew</b>', '"', '\'', new Date, Date, Math, 42, 8, -1, 0, false, true];
-
-        return data;
-    }
-};
